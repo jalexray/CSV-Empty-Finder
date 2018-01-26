a0acbf44ea6e645e78490671eb0a33e5aa9ac3f3
Alex Ray <jalexray@gmail.com>
2018-01-25 23:25:47 -0600

    second commit

--- main.py
+++ main.py
@@ -2,15 +2,29 @@
 import csv
 
 # open the file
+
 with open("example.csv") as file:
 	reader = csv.reader(file)
 	# prep to store names of columns
 	titleRow = reader.next()
    	#rest = [row for row in reader]
-
+   	columnList = {}
 	for row in reader:
 		iterator = 0
+		cellList = []
 		for cell in row:
 			if cell == "":
-				print(titleRow[iterator],row[0])
+				cellList.append(titleRow[iterator])
+				#print("Within " + row[0] + ", " + titleRow[iterator] + " has an empty")
 			iterator += 1 
+		#print cellList
+		columnList[row[0]] = cellList
+
+for item in sorted(columnList):
+	itemString = (str(columnList[item]))
+	if itemString != "[]":
+		print(item + ":" + str(columnList[item]))
+
+
+
+
